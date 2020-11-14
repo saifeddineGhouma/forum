b1341b04247ef0da553fe3a44c4d26e414d210c0
saifeddine ghouma <s.ghouma@oxton-digital.com>
2020-11-14 22:38:10 +0100

    add data to database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Model\Question;
+use App\Model\Reply ; 
+use App\Model\Like;
+use App\Model\Category;
+use App\User ;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(User::class,10)->create();
+        factory(Category::class,10)->create();
+        factory(Question::class,10)->create();
+        factory(Reply::class, 10)->create()->each(function($reply){
+            return $reply->likes()->save(factory(Like::class)->make());
+        });
+
     }
 }
